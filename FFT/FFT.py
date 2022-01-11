8724271186c36c68924da2d3a832d324d76c649e
LonelVino <lonelvino@gmail.com>
2022-01-11 12:35:08 +0100

    add fluteircam.txt

--- FFT/FFT.py
+++ FFT/FFT.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # %% Main Part
 if __name__ == '__main__':
-    filename = 'voiceP.wav'; root = 'assets/audio/'
-    window_period = 0.2
+    filename = 'fluteircam.wav'; root = 'assets/audio/'
+    window_period = 0.1
     f0 = FFT_f0(window_period, filename, root)
     
     print('\n', '_'*80, '\n[INFO] Read Audio and Perform FFT', )
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     signal_restored = Restore_model.restore(FFT_array)
     signal_processed = Restore_model.signal_process(signal_restored, signal_original)
     Restore_model.compare_signal(total_time, signal_original, signal_restored, signal_processed)
-    Restore_model.save_audio(signal_processed, save_path, is_play=True)
+    Restore_model.save_audio(signal_processed, save_path, is_play=False)
     plt.show()
     
